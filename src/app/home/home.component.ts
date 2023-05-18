e00667f1e7a4992d50c4e179d37b0f69a090a360
Czakus <krusik.szymon@gmail.com>
2023-05-18 20:46:09 +0200

    Sekcja 11/151 - Prawie koniec routingu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
+
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  login() {
+    this.authService.logIn();
+  }
+
+  logout() {
+    this.authService.logOut();
+  }
 
   ngOnInit() {
   }
